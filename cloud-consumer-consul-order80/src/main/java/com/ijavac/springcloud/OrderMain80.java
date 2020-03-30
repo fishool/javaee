1d3ccbef28003b4995c2653cf8cfc7f55e1717c7
ijavacs <ijavacs@gmail.com>
2020-03-30 17:31:15 +0800

    spring_cloud_day002

---
+++ cloud-consumer-consul-order80/src/main/java/com/ijavac/springcloud/OrderMain80.java
@@ -0,0 +1,17 @@
+package com.ijavac.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName : PaymentMain8001
+ * @Description : 启动类
+ * @Author : ijavac
+ * @date: 2020-03-30 14:39
+ */
+@SpringBootApplication
+public class OrderMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain80.class);
+    }
+}
