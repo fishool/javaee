71f951ae9e4dab813c5bdaeb92f5b027004dc2eb
ijavacs <ijavacs@gmail.com>
2020-04-01 21:51:19 +0800

    spring_cloud_day004

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/ijavac/springcloud/service/PaymentHystrixService.java
@@ -0,0 +1,37 @@
+package com.ijavac.springcloud.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @author zzyy
+ * @create 2020/3/6 23:19
+ *
+ * @FeignClient(value = "CLOUD-PROVIDER-HYSTRIX-PAYMENT",fallback = PaymentFallbackService.class)
+ * 对整个service调用进行服务降级
+ **/
+@Component
+@FeignClient(value = "CLOUD-PROVIDER-HYSTRIX-PAYMENT",fallback = PaymentFallbackService.class)
+public interface PaymentHystrixService {
+
+
+    /**
+     * 正常访问
+     * @param id
+     * @return
+     */
+    @GetMapping("/payment/hystrix/ok/{id}")
+    String paymentInfo_OK(@PathVariable("id") Integer id);
+
+    /**
+     * 超时访问
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/payment/hystrix/timeout/{id}")
+    String paymentInfo_TimeOut(@PathVariable("id") Integer id);
+
+}
--- cloud-consumer-order80/src/main/java/com/ijavac/myrule/MySelfRule.java
+++ cloud-consumer-order80/src/main/java/com/ijavac/myrule/MySelfRule.java
@@ -1,7 +1,7 @@
 package com.ijavac.myrule;
 
 import com.netflix.loadbalancer.IRule;
-import com.netflix.loadbalancer.RoundRobinRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,9 +14,33 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MySelfRule {
 
+//    @Bean
+//    public IRule myRule() {
+//        // 复核判断可用性和性能。
+//        return new ZoneAvoidanceRule();
+//    }
+
+
+//    @Bean
+//    public IRule myRule() {
+//        // 定义为轮训  默认出厂
+//        return new RoundRobinRule();
+//
+//    }
+
     @Bean
     public IRule myRule() {
         // 定义为随机
-        return new RoundRobinRule();
+        return new RandomRule();
+
     }
+
+//    @Bean
+//    public IRule myRule() {
+//        // 定义高可用优先
+//        return new BestAvailableRule();
+//    }
+//
+
+
 }
